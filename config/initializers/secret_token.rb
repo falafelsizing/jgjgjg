dbdfc773ca53a7a85e4c5d74fa9f6f69f52eb050
Scott Vijay Gehlot <scott.vijay.gehlot@gmail.com>
2012-10-23 22:24:26 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jg::Application.config.secret_token = '3bcc9dc345fc456bcde20a7de5c7d724093ee562d58b84c426ad0099824275887eeddc779450a0ade8471a0ffc5e0adbf2a60b970fbafc2621b7cc406d7f67a1'
